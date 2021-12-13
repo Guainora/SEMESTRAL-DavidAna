e49c2140652d5973cc19a972fd710a8e93ebebaf
davidope32 <76079430+davidope32@users.noreply.github.com>
2021-12-13 17:30:33 -0500

    integracion codigo interfaz

--- app/src/main/java/org/example/proyectosemestraldavidpanag/MainActivity.java
+++ app/src/main/java/org/example/proyectosemestraldavidpanag/MainActivity.java
@@ -1,14 +1,138 @@
 package org.example.proyectosemestraldavidpanag;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-// prueba 1 subida comentario
-public class MainActivity extends AppCompatActivity {
+import android.widget.ImageView;
+import android.widget.TextView;
 
+public class MainActivity extends AppCompatActivity implements  SensorEventListener{
+
+    TextView txtx,txty,txtz;
+    Sensor proximidad, lacelerometro;
+    ImageView img;
+    SensorManager sensorManager;
+    long lastUpdate = 0;
+    float last_x, last_y, last_z;
+    static final int SHAKE_THRESHOLD = 800;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtx=findViewById(R.id.ejeX);
+        txty=findViewById(R.id.ejeY);
+        txtz=findViewById(R.id.ejeZ);
+        img= findViewById(R.id.img);
+
+
+
+        sensorManager= (SensorManager) getSystemService(SENSOR_SERVICE);
+        proximidad=sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        lacelerometro=sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        sensorManager.registerListener(this, proximidad, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, lacelerometro, SensorManager.SENSOR_DELAY_NORMAL);
+
+
+
+
+
+
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+
+        switch (sensorEvent.sensor.getType()){
+            case Sensor.TYPE_PROXIMITY:
+                if(sensorEvent.values[0]<proximidad.getMaximumRange()){
+                    img.setImageResource(R.drawable.acelerometro);
+
+                }else
+                {
+                    img.setImageResource(R.drawable.acelerometro2);
+                }
+                break;
+
+            case Sensor.TYPE_LINEAR_ACCELERATION:
+                float x = sensorEvent.values[0];
+                float y = sensorEvent.values[1];
+                float z = sensorEvent.values[2];
+
+                long curTime = System.currentTimeMillis();
+
+                if ((curTime - lastUpdate) > 100) {
+                    long diffTime = (curTime - lastUpdate);
+                    lastUpdate = curTime;
+
+                    float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
+
+                    if(speed >SHAKE_THRESHOLD)
+                    {
+
+                    }
+
+
+                    last_x = x;
+                    last_y = y;
+                    last_z = z;
+
+                    if(last_x >1){
+                        sonido();
+                    }
+
+                    txtx.setText(""+last_x+" m/s2");
+                    txty.setText(""+last_y+" m/s2");
+                    txtz.setText(""+last_z+" m/s2");
+
+                }
+
+
+
+
+                break;
+
+
+
+            default:
+                break;
+
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        sensorManager.registerListener(this, proximidad, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    private void sonido()
+    {
+        MediaPlayer mediaPlayer= MediaPlayer.create(this,R.raw.song);
+
     }
 }
+
+
+
